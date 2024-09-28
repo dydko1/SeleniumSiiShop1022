fef8039075dec10b680cbf25111e3b8b68923bfc
dydko <miroslaw.dyduch@gmail.com>
2024-09-28 07:28:10 +0200

    remote driver, selenoid, vnc

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -6,8 +6,13 @@ import org.assertj.core.api.SoftAssertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 
 public class TestBase {
     public WebDriver driver;
@@ -20,8 +25,37 @@ public class TestBase {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
         options.addArguments("--remote-allow-origins=*");
+        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+            //put("enableVideo", true);
+            put("enableVNC", true); // add this key!!! see
+            /*
+            !!!!! http://192.168.10.61:8080/#/capabilities/ !!!!!
 
-        driver = new ChromeDriver(options);
+            ChromeOptions options = new ChromeOptions();
+            options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+            put("name", "Test badge...");
+
+            put("sessionTimeout", "15m");
+
+            put("env", new ArrayList<String>() {{
+                add("TZ=UTC");
+            }});
+
+            put("labels", new HashMap<String, Object>() {{
+                put("manual", "true");
+            }});
+
+            put("enableVideo", true);
+        }});
+        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://192.168.10.61:4444/wd/hub"), options);
+             */
+        }});
+
+        try {
+            driver = new RemoteWebDriver(new URL("http://192.168.10.61:4444/wd/hub"), options);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
         driver.get(UrlProvider.app);
     }
 
