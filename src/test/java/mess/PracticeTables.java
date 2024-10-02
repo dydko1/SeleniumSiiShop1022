adda17a86ff9a5e91c024e41e17a71312543a40d
dydko <miroslaw.dyduch@gmail.com>
2024-10-02 15:39:16 +0200

    mess 123

---
+++ src/test/java/mess/PracticeTables.java
@@ -0,0 +1,25 @@
+package mess;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class PracticeTables {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://toolsqa.com/automation-practice-table");
+
+        //Here we are storing the value from the cell in to the string variable
+        String sCellValue = driver.findElement(By.xpath(".//*[@id='content']/table/tbody/tr[1]/td[2]")).getText();
+        System.out.println(sCellValue);
+
+        // Here we are clicking on the link of first row and the last column
+        driver.findElement(By.xpath(".//*[@id='content']/table/tbody/tr[1]/td[6]/a")).click();
+        System.out.println("Link has been clicked otherwise an exception would have thrown");
+        driver.close();
+    }
+}
